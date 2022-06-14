4d78dbe7b2d6d7eec55898bb924be872eeb42ff6
Dela Cruz <ron.e.dela.cruz@accenture.com>
2022-06-14 14:38:22 +0800

    Init commit

---
+++ src/app.js
@@ -0,0 +1,105 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+//define path
+const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//setup handlebars
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//setup static 
+app.use(express.static(publicDirPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Ron Ezekiel'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Ron Ezekiel'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        msg: 'Hello you need help?',
+        title: 'Help',
+        name: 'Ron Ezekiel'
+    })
+})
+
+app.get('/weather', (req, res) => {
+
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    } else {
+        geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            forecast(latitude, longitude, (error, forecastData) => {
+
+                if (error) {
+                    return res.send({ error })
+                }
+
+                res.send({
+                    forecast: forecastData,
+                    location,
+                    address: req.query.address
+                })
+
+            })
+        })
+    }
+})
+
+app.get('/products', (req, res) => {
+
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        msg: '404 Help article not found',
+        title: '404',
+        name: 'Ron Ezekiel'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        msg: '404 Page not found',
+        title: '404',
+        name: 'Ron Ezekiel'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+
+    const url = 'http://api.weatherstack.com/current?access_key=d5178d60a9339c271b446ecbc2de532b&query=' + longitude + ',' + latitude + ''
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location. Try another search', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions + '. The temperature is ' + body.current.temperature + ' degrees and The chance of rain is ' + body.current.precip + '%.')
+        }
+    })
+}
+
+module.exports = forecast
